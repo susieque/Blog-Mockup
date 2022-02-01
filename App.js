cce68c648f7c00d643cec833f5b311d3cd90a47d
Susie Riley <susie09rob@gmail.com>
2022-02-01 11:43:21 -0800

    Updating UseReducer

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import BlogContext from './src/context/BlogContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
   },
-});
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs',
+    },
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` },
+    ]);
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      <App />
+    </BlogContext.Provider>
+  );
+};
+
---
+++ src/context/BlogContext.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react';
+
+// From Blog Post Provider to BLOGLIST
+const BlogContext = React.createContext();
+
+const blogReducer = (state, action) => {
+	switch (action.type) {
+		case 'add_blogpost':
+			return [...state, { title: `Blog Post #${state.length + 1}` }];
+		default:
+			return state;
+	}
+};
+
+export const BlogProvider = ({ children }) => {
+	const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+	const addBlogPost = () => {
+		dispatch({ type: 'add_blogpost' });
+	};
+	return (
+		<BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+			{children}
+		</BlogContext.Provider>
+	);
+};
+
+export default BlogContext;
