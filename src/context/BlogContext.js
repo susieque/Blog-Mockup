d3f7ee2bba84216c697d553de8681f8e092215ca
Susie Riley <susie09rob@gmail.com>
2022-02-11 14:37:09 -0800

    Deleting Posts and Update Reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,22 +1,35 @@
 import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
-  switch (action.type) {
-    case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case 'delete_blogpost':
+			return state.filter((blogPost) => blogPost.id !== action.payload);
+		case 'add_blogpost':
+			return [
+				...state,
+				{
+					id: Math.floor(Math.random() * 99999),
+					title: `Blog Post #${state.length + 1}`
+				}
+			];
+		default:
+			return state;
+	}
 };
 
-const addBlogPost = dispatch => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
-  };
+const addBlogPost = (dispatch) => {
+	return () => {
+		dispatch({ type: 'add_blogpost' });
+	};
+};
+const deleteBlogPost = (dispatch) => {
+	return (id) => {
+		dispatch({ type: 'delete_blogpost', payload: id });
+	};
 };
 
 export const { Context, Provider } = createDataContext(
-  blogReducer,
-  { addBlogPost },
-  []
+	blogReducer,
+	{ addBlogPost, deleteBlogPost },
+	[]
 );
